8aaef372057b9d9fa8aaa922dfbd54656b28a420
Stephan Knitelius <stephan@knitelius.com>
2014-06-10 17:23:02 +0200

    moved ApplicationConfig

--- src/main/java/com/knitelius/basic/angularjsee/application/config/ApplicationConfig.java
+++ src/main/java/com/knitelius/basic/angularjsee/application/config/ApplicationConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.knitelius.basic.angularjsee.rest.application.config;
+package com.knitelius.basic.angularjsee.application.config;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
